977f319e4c6eebcef4fdee977805e3eedf78a292
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-08-21 21:43:50 -0300

    Add asset manager

--- core/src/com/emmanuelmess/simplechess/SimpleChessGame.kt
+++ core/src/com/emmanuelmess/simplechess/SimpleChessGame.kt
@@ -2,16 +2,21 @@ package com.emmanuelmess.simplechess
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 class SimpleChessGame : ApplicationAdapter() {
-
+    private lateinit var assetManager: AssetManager
 
     var batch: SpriteBatch? = null
     var img: Texture? = null
     override fun create() {
+        assetManager = AssetManager().apply {
+            
+        }
+
         batch = SpriteBatch()
         img = Texture("badlogic.jpg")
     }
